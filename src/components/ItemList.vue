6974ab27bc7b410c3c14d1358ce510410bc31cab
noubuddy <anton.nich00@gmail.com>
2021-11-30 09:04:56 +0200

    Changes

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -1,21 +1,20 @@
 <template>
-    <div>
-        <h1><slot></slot></h1>
-        <ul>
-            <li v-for="item in items" :key="item.id">
-                <input type="checkbox" v-model="item.isDone">
-                {{item.name}}
-            </li>
-        </ul>
-    </div>
+  <div>
+    <h1><slot></slot></h1>
+    <ul>
+      <li v-for="item in items" :key="item.id">
+        <input type="checkbox" v-model="item.isDone" />
+        {{ item.name }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-    props: ['items']
-}
+  props: ["items"],
+};
 </script>
 
 <style>
-
 </style>
